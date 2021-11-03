2cf1b471d3e566a92f93b24f4afce4c8c767eae8
Hans Lemuet <Spone@users.noreply.github.com>
2021-11-03 13:17:27 +0100

    Update app/templates/install/index.stimulus.js

--- app/templates/install/index.stimulus.js
+++ app/templates/install/index.stimulus.js
@@ -14,7 +14,7 @@ context.keys().forEach((path) => {
   //   nav/user_info/index.js -> nav--user-info
   const identifier = path.replace(/^\\.\\//, '')
     .replace(/\\/index\\.js$/, '')
-    .replace(/\//g, '--');
+    .replace(/\\//g, '--');
 
   application.register(identifier, mod.Controller);
 });
